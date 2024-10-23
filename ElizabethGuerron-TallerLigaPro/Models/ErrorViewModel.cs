075ef30166bc4dd18aaaccac4e18000f3ab1d131
e1iguerron789 <130955581+e1iguerron789@users.noreply.github.com>
2024-10-23 10:40:04 -0500

    Agregar archivos de proyecto.

---
+++ ElizabethGuerron-TallerLigaPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElizabethGuerron_TallerLigaPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
